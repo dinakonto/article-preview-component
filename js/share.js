6046473152eefb3d74f28b09e55e5f50105a00ab
Dina Konto <dina.konto@gmail.com>
2020-08-11 12:03:47 +1000

    Initial commit - basic styling and functionality

---
+++ js/share.js
@@ -0,0 +1,9 @@
+const shareBtn = $(".share-btn");
+const area = $(".meta");
+const byline = $(".byline");
+const share = $(".share")
+const shareOptions = $(".share-options");
+
+shareBtn.click(function() {
+  shareOptions.toggle();
+})

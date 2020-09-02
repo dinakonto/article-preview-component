8450bb89981a7b7e7d3e38ca1cc35b8d372d848a
Dina Konto <dina.konto@gmail.com>
2020-09-02 11:27:56 +1000

    Remove TODOs

--- js/share.js
+++ js/share.js
@@ -1,15 +1,4 @@
-/* QUESTIONS / TODO :
- * 1. How can I make sure shareOptions stays fully visible
- * even when the viewport is a bit small (e.g. 880px)?
- * Want to stick to the right until screen is wide enough,
- * AND for the arrow to always be above the btn regardless.
- *
- * 2. Click outside share box to hide
- *
- * 3. Animate shareOptions backgroundColor/appearance
- */
-
-const shareBtn = $(".share-btn")
+const shareBtn = $(".share-btn");
 const shareOptions = $(".share-options");
 
 shareBtn.click(function() {
